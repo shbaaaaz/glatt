c1289c4bc21c31d4c7c234d22c9d47d3856fdd39
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2023-12-07 16:55:36 +0100

    feat: added cart counter badge

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,11 @@ import { FaUserCircle } from 'react-icons/fa'
 
 const Header = () => {
   const location = 'Cologne'
+  const itemsInCart = 0
   return (
     <nav className={styles['nav']}>
       <div className={styles['brand-container']}>
-        <a className={styles['brand']}>Glattessen</a>
+        <a className={styles['brand']}>GlattEssen</a>
         <div className={styles['location']}>
           <span>
             <IoLocationSharp />
@@ -21,8 +22,9 @@ const Header = () => {
           <SearchBar />
         </div>
         <div className={styles['nav-cta-group']}>
-          <div>
+          <div className={styles['cart']}>
             <IoBagHandle role='button' className={styles['icon-button']} />
+            <span className={styles['cart-counter']}>{itemsInCart}</span>
           </div>
           <div>
             <FaUserCircle role='button' className={styles['icon-button']} />
